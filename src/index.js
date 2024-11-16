d5a0a6d606fb65fae5bc61975cabd678cd9869ce
RachitShahrawat <rachitshahrawat@gmail.com>
2024-11-16 23:49:48 +0530

    Added unique constraints and started repository

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const bodyParser=require("body-parser");
-
 const { PORT } = require('./config/serverconfig');
+
 
 
 const setupAndStartServer=async()=>{
@@ -11,7 +11,7 @@ const setupAndStartServer=async()=>{
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     
-    app.listen(PORT,()=>{
+    app.listen(PORT,async()=>{
         console.log(`Server started at ${PORT}`);
     })
 }
