c6a8b37a04f5567081d69edf9d4c84fc417bf5c2
RachitShahrawat <rachitshahrawat@gmail.com>
2024-12-04 16:06:21 +0530

    done minor changes in routes

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -6,4 +6,8 @@ const router=express.Router();
 
 router.post('/city',CityController.create); // whenever someone will call /v1/city we will call city controller function
 
+router.delete('/city/:id',CityController.destroy);
+router.get('/city/:id',CityController.get);
+router.patch('/city/:id',CityController.update);
+
 module.exports=router;
