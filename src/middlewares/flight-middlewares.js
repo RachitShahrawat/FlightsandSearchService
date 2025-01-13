9096186e93cd22c5a48021f3538c9d23d52abeb0
RachitShahrawat <rachitshahrawat@gmail.com>
2025-01-13 17:29:45 +0530

    added error code

--- src/middlewares/flight-middlewares.js
+++ src/middlewares/flight-middlewares.js
@@ -1,3 +1,6 @@
+const {ClientErrorsCodes}=require('../utils/error-codes');
+
+
 const validateCreateFlight=(req,res,next)=>{
     if(!req.body.flightNumber ||
         !req.body.airplaneId ||
@@ -6,7 +9,7 @@ const validateCreateFlight=(req,res,next)=>{
         !req.body.arrivaltime ||
         !req.body.departuretime || !req.body.price
     ){
-        return res.status(400).json({
+        return res.status(ClientErrorsCodes.BAD_REQUEST).json({
             data:{},
             success:false,
             message:'Invalid request body for create flight',
